063369b002950d5838e59d6c5560cbd39da420fb
taichi256 <104846948+taichi256@users.noreply.github.com>
2024-03-13 17:47:23 +0900

    ChooseColumn
    javascriptで頑張って文字の大きさ調整するようにした。

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask,render_template,redirect,request,url_for,session
+from flask import Flask, render_template, redirect, request, url_for, session
 import pandas as pd
 from modules import createID
 import time
 from model.models import model_lightgbm
 
-app = Flask(__name__,static_folder="./static")
-
+app = Flask(__name__, static_folder="./static")
 
 
 dataDict = {}
@@ -15,7 +14,8 @@ dataDict = {}
 
 allowedID = set([])
 
-@app.route("/",methods=["POST","GET"])
+
+@app.route("/", methods=["POST", "GET"])
 def index():
     global allowedID
     id = createID.getID()
@@ -23,10 +23,10 @@ def index():
         id = createID.getID()
     allowedID.add(id)
     if request.method == "GET":
-        return render_template("index.html",id=id)
+        return render_template("index.html", id=id)
 
 
-@app.route("/<string:id>/home",methods=["GET","POST"])
+@app.route("/<string:id>/home", methods=["GET", "POST"])
 def home(id):
     if id not in allowedID:
         return redirect("/")
@@ -39,11 +39,11 @@ def home(id):
         #'.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
         print(file)
         df = pd.read_csv(file)
-        dataDict[id] = [time.time(),df]
+        dataDict[id] = [time.time(), df]
         return redirect(f"/{id}/choseTarget")
-    
 
-@app.route("/<string:id>/choseTarget",methods = ["GET","POST"])
+
+@app.route("/<string:id>/choseTarget", methods=["GET", "POST"])
 def choseTarget(id):
     if id not in allowedID:
         return redirect("/")
@@ -53,18 +53,15 @@ def choseTarget(id):
         data = []
         for i in range(len(df)):
             data.append(list(df.iloc[i]))
-        return render_template("choseTarget.html",columns=columns,data=data)
+        return render_template("chooseColumn.html", columns=columns, data=data)
     else:
         checkedItem = request.form.getlist("checkbox")
         df = df[checkedItem]
         LGBM = model_lightgbm()
-        LGBM.learning(analytic_type="C",target="Target",df = df)
+        LGBM.learning(analytic_type="C", target="Target", df=df)
         accuracy = LGBM.accuracy
-        return render_template("score.html",accuracy = accuracy)
-
-        
+        return render_template("score.html", accuracy=accuracy)
 
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
