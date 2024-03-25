a1f6409b57a5463fcd6765ecff69f7560577029a
taichi256 <104846948+taichi256@users.noreply.github.com>
2024-03-25 12:37:44 +0900

    衝突を無理やり防ぐ

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from flask import (
-    Flask,
-    render_template,
-    redirect,
-    request,
-    url_for,
-    session,
-    make_response,
-)
+from flask import Flask,render_template,redirect,request,url_for,session,make_response
 import pandas as pd
 from modules import createID
 import time
@@ -14,7 +6,7 @@ from model.models import model_lightgbm
 import math
 
 
-app = Flask(__name__, static_folder="./static")
+app = Flask(__name__,static_folder="./static")
 
 
 dataDictTrain = {}
@@ -26,13 +18,12 @@ dataDictTest = {}
 # value:[timestamp,df]
 
 modelDict = {}
-# key:id
-# value:[timestamp,imp,x_columns]
+#key:id
+#value:[timestamp,imp,x_columns]
 
 allowedID = set([])
 
-
-@app.route("/", methods=["POST", "GET"])
+@app.route("/",methods=["POST","GET"])
 def index():
     global allowedID
     id = createID.getID()
@@ -40,10 +31,10 @@ def index():
         id = createID.getID()
     allowedID.add(id)
     if request.method == "GET":
-        return render_template("index.html", id=id)
+        return render_template("index.html",id=id)
 
 
-@app.route("/<string:id>/home", methods=["GET", "POST"])
+@app.route("/<string:id>/home",methods=["GET","POST"])
 def home(id):
     if id not in allowedID:
         return redirect("/")
@@ -55,15 +46,15 @@ def home(id):
         trainFile = request.files["trainingCSVfile"]
         testFile = request.files["testCSVfile"]
         #'.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
+        
         dfTrain = pd.read_csv(trainFile)
-        dataDictTrain[id] = [time.time(), dfTrain]
+        dataDictTrain[id] = [time.time(),dfTrain]
         dfTest = pd.read_csv(testFile)
-        dataDictTest[id] = [time.time(), dfTest]
+        dataDictTest[id] = [time.time(),dfTest]
         return redirect(f"/{id}/choseTarget")
+    
 
-
-@app.route("/<string:id>/choseTarget", methods=["GET", "POST"])
+@app.route("/<string:id>/choseTarget",methods = ["GET","POST"])
 def choseTarget(id):
     if id not in allowedID:
         return redirect("/")
@@ -74,10 +65,10 @@ def choseTarget(id):
         data = []
         for i in range(len(dfTrain)):
             data.append(list(dfTrain.iloc[i]))
-        return render_template("choseTarget.html", columns=columns, data=data)
+        return render_template("choseTarget.html",columns=columns,data=data)
     else:
-        # checkedItem = request.form.getlist("checkbox")
-        # df = df[checkedItem]
+        #checkedItem = request.form.getlist("checkbox")
+        #df = df[checkedItem]
         print(request.form)
         target = request.form["item"]
         analyticType = request.form["selection"]
@@ -87,29 +78,24 @@ def choseTarget(id):
             analyticType = "L"
 
         LGBM = model_lightgbm()
-        LGBM.learning(analytic_type="C", target=target, dfTrain=dfTrain, dfTest=dfTest)
-        modelDict[id] = [time.time(), LGBM]
+        LGBM.learning(analytic_type="C",target=target,dfTrain = dfTrain,dfTest=dfTest)
+        modelDict[id] = [time.time(),LGBM]
         return redirect(f"/{id}/score")
 
 
-@app.route("/<string:id>/score", methods=["GET", "POST"])
+@app.route("/<string:id>/score",methods=["GET","POST"])
 def score(id):
     if id not in allowedID:
         return redirect("/")
     if request.method == "GET":
         LGBM = modelDict[id][1]
-        return render_template(
-            "score.html",
-            accuracy=LGBM.accuracy,
-            accpct=math.floor(LGBM.accuracy * 100),
-            imp=LGBM.imp,
-            columns=LGBM.columns,
-        )
+        return render_template("score.html",accuracy = LGBM.accuracy, accpct = math.floor(LGBM.accuracy*100),imp=LGBM.imp,columns = LGBM.columns)
     else:
         pass
 
 
-@app.route("/<string:id>/download", methods=["GET", "POST"])
+
+@app.route("/<string:id>/download",methods = ["GET","POST"])
 def download(id):
     if id not in allowedID:
         return redirect("/")
@@ -118,13 +104,12 @@ def download(id):
     csv = df.to_csv(index=False)
 
     response = make_response(csv)
-    cd = "attachment; filename=data.csv"
-    response.headers["Content-Disposition"] = cd
-    response.mimetype = "text/csv"
+    cd = 'attachment; filename=data.csv'
+    response.headers['Content-Disposition'] = cd 
+    response.mimetype='text/csv'
     return response
 
-
-@app.route("/<string:id>/predict", methods=["GET", "POST"])
+@app.route("/<string:id>/predict",methods = ["GET","POST"])
 def predict(id):
     if id not in allowedID:
         return redirect("/")
@@ -132,8 +117,10 @@ def predict(id):
     if request.method == "GET":
         LGBM = modelDict[id][1]
         LGBM.predict()
-        return render_template("result.html", id=id)
+        return render_template("result.html",id = id)
 
 
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
     app.run()
