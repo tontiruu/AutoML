6b0fcd1e0c593ebfd82ff1a74570a298e0bc227f
taichi256 <104846948+taichi256@users.noreply.github.com>
2024-03-25 12:17:56 +0900

    revert main

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from flask import Flask, render_template, redirect, request, url_for, session
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    request,
+    url_for,
+    session,
+    make_response,
+)
 import pandas as pd
 from modules import createID
 import time
 from model.models import model_lightgbm
+import math
+
 
 app = Flask(__name__, static_folder="./static")
 
 
-dataDict = {}
+dataDictTrain = {}
 # key:id
 # value:[timestamp,df]
 
+dataDictTest = {}
+# key:id
+# value:[timestamp,df]
+
+modelDict = {}
+# key:id
+# value:[timestamp,imp,x_columns]
 
 allowedID = set([])
 
@@ -19,7 +36,7 @@ allowedID = set([])
 def index():
     global allowedID
     id = createID.getID()
-    while id in dataDict.keys():
+    while id in dataDictTrain.keys():
         id = createID.getID()
     allowedID.add(id)
     if request.method == "GET":
@@ -33,13 +50,16 @@ def home(id):
     if request.method == "GET":
         return render_template("home.html")
     else:
-        global dataDict
+        global dataDictTrain, dataDictTest
         print(request.files)
-        file = request.files["CSVfile"]
+        trainFile = request.files["trainingCSVfile"]
+        testFile = request.files["testCSVfile"]
         #'.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-        print(file)
-        df = pd.read_csv(file)
-        dataDict[id] = [time.time(), df]
+
+        dfTrain = pd.read_csv(trainFile)
+        dataDictTrain[id] = [time.time(), dfTrain]
+        dfTest = pd.read_csv(testFile)
+        dataDictTest[id] = [time.time(), dfTest]
         return redirect(f"/{id}/choseTarget")
 
 
@@ -47,21 +67,73 @@ def home(id):
 def choseTarget(id):
     if id not in allowedID:
         return redirect("/")
-    df = dataDict[id][1]
+    dfTrain = dataDictTrain[id][1]
+    dfTest = dataDictTest[id][1]
     if request.method == "GET":
-        columns = df.columns
+        columns = dfTrain.columns
         data = []
-        for i in range(len(df)):
-            data.append(list(df.iloc[i]))
-        return render_template("chooseColumn.html", columns=list(columns), data=data)
+        for i in range(len(dfTrain)):
+            data.append(list(dfTrain.iloc[i]))
+        return render_template("choseTarget.html", columns=columns, data=data)
     else:
-        checkedItem = request.form.getlist("checkbox")
-        df = df[checkedItem]
+        # checkedItem = request.form.getlist("checkbox")
+        # df = df[checkedItem]
+        print(request.form)
+        target = request.form["item"]
+        analyticType = request.form["selection"]
+        if analyticType == "分類":
+            analyticType = "C"
+        else:
+            analyticType = "L"
+
         LGBM = model_lightgbm()
-        LGBM.learning(analytic_type="C", target="Target", df=df)
-        accuracy = LGBM.accuracy
-        return render_template("score.html", accuracy=accuracy)
+        LGBM.learning(analytic_type="C", target=target, dfTrain=dfTrain, dfTest=dfTest)
+        modelDict[id] = [time.time(), LGBM]
+        return redirect(f"/{id}/score")
+
+
+@app.route("/<string:id>/score", methods=["GET", "POST"])
+def score(id):
+    if id not in allowedID:
+        return redirect("/")
+    if request.method == "GET":
+        LGBM = modelDict[id][1]
+        return render_template(
+            "score.html",
+            accuracy=LGBM.accuracy,
+            accpct=math.floor(LGBM.accuracy * 100),
+            imp=LGBM.imp,
+            columns=LGBM.columns,
+        )
+    else:
+        pass
+
+
+@app.route("/<string:id>/download", methods=["GET", "POST"])
+def download(id):
+    if id not in allowedID:
+        return redirect("/")
+    LGBM = modelDict[id][1]
+    df = LGBM.pred_df
+    csv = df.to_csv(index=False)
+
+    response = make_response(csv)
+    cd = "attachment; filename=data.csv"
+    response.headers["Content-Disposition"] = cd
+    response.mimetype = "text/csv"
+    return response
+
+
+@app.route("/<string:id>/predict", methods=["GET", "POST"])
+def predict(id):
+    if id not in allowedID:
+        return redirect("/")
+    dfTest = dataDictTest[id][1]
+    if request.method == "GET":
+        LGBM = modelDict[id][1]
+        LGBM.predict()
+        return render_template("result.html", id=id)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
